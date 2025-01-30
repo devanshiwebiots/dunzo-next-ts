6ebb3c10578ce9c97f668d67378bc68d6e7fbe7d
devanshiwebiots <devanshi.shah@webiots.com>
2025-01-30 14:55:21 +0530

    push

--- src/Components/Auth/UserForm.tsx
+++ src/Components/Auth/UserForm.tsx
@@ -22,7 +22,7 @@ const UserForm = () => {
     const result = await signIn("credentials", {
       email,   
       password,  
-      redirect: false,
+      redirect: true,
       callbackUrl: "/dashboard/default",
     });
   
